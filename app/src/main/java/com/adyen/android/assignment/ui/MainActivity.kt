63a13c74aa4d209abf9b8c465bf2352b72987d5d
Shamil Magomedov <shamil10031@mail.ru>
2024-10-12 11:55:30 +0200

    Init commit

---
+++ app/src/main/java/com/adyen/android/assignment/ui/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.adyen.android.assignment.ui
+
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    // TODO: Placeholder activity that may be used
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+@file:Suppress("UnstableApiUsage")
+
+rootProject.name = "Assignment"
+include(":app")
+include(":cashregister")
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
